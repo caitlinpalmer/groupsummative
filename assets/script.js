dc0f52f4916a0c90c43e24b7f1741d51a3e81981
ma mica trixia busog encela <932400139@cit3-501m-18.acg.ac.nz>
2018-09-14 16:21:25 +1200

    worked on maps css

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+// const version = '?v=20170901';
+// const clientid = '&client_id=KSZ4O3RILUSKALBIFZ4SZBEITPNBF5MEYYKWJWIYAJPUKMMF';
+// const clientSecret = '&client_secret=TB2XI0VW42BD3PDTZ2SGTQWWUCHWFAL0PTKLDCVGHCD33Q4B';
+// const key = version + clientid + clientSecret;
+
+$(function(){
+
+	let center = [-36.8446152873055,174.76662397384644];
+	let map = L.map('map').setView(center,17);
+	L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoidGhhbHl4OTAiLCJhIjoiY2o2YjdrZHRlMWJmYjJybDd2cW1rYnVnNSJ9.j_DQLfixHfhioVjH6qmqkw').addTo(map);
+});
