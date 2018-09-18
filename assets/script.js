f3a3dd0d678b6a40f7d49cda9744928b14645e1f
caitlin rose palmer <932400011@cit3-501m-19.acg.ac.nz>
2018-09-18 15:37:10 +1200

    modal pop up started

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,6 @@
 const version = '?v=20170901';
-const clientid = '&client_id=PJQQYBGMFJ0NV2QRF441L4ZQ03ZFDFPYAZZWLVTYQSLFEBBZ';
-const clientSecret = '&client_secret=CJRASI3SGKB33A5QTC5V5MRJ1Q2T51E3JCSIVLDLEBPIGV2I';
+const clientid = '&client_id=V411NRDVQFIXJDGXGIRRE4X3UEW25UNF0AV30STXIE1B3U1N';
+const clientSecret = '&client_secret=SDYHGPFRXFDHHB5JKV5I0R0YXEZYNF1XZQZHJBTBWO3TM4Z2';
 const key = version + clientid + clientSecret;
 
 var map;
@@ -8,14 +8,20 @@ var map;
 var categories = {
 	food: '4d4b7105d754a06374d81259',
 	transport: '52f2ab2ebcbc57f1066b8b4f',
-	accommodation: '4bf58dd8d48988d1fa931735'
+	accommodation: '4bf58dd8d48988d1fa931735',
+	shopping: '4bf58dd8d48988d1fd941735',
+	banks: '4bf58dd8d48988d10a951735',
+	activities: '4d4b7104d754a06370d81259'
 }
 
 
 var icons = {
 	food: 'assets/images/pinkdot.svg',
 	transport: 'assets/images/purpledot.svg',
-	accommodation: 'assets/images/bluedot.svg'
+	accommodation: 'assets/images/bluedot.svg',
+	shopping: 'assets/images/greendot.svg',
+	banks: 'assets/images/orangedot.svg',
+	activities: 'assets/images/yellowdot.svg'
 }
 
 $(function(){
@@ -23,7 +29,6 @@ $(function(){
 	let center = [-36.848953,174.762573];
 	map = L.map('map').setView(center,15);
 	L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v9/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiY2FpdGxpbnBhbG1lciIsImEiOiJjamtrZXlsaW4xcHE5M3FtbDEwbzBqdG92In0.RY6_Y65ouMFFFrT6B6Mvww').addTo(map);
-
 
 	//switching of layers
 	$('.fa-search').on('click',function(){
@@ -39,6 +44,9 @@ $(function(){
 	getVenues(ll,categories.food,icons.food);
 	getVenues(ll,categories.transport,icons.transport);
 	getVenues(ll,categories.accommodation,icons.accommodation);
+	getVenues(ll,categories.shopping,icons.shopping);
+	getVenues(ll,categories.banks,icons.banks);
+	getVenues(ll,categories.activities,icons.activities);
 
 
 });
@@ -58,7 +66,8 @@ function getVenues(location,category,icon){
 			var venues = _(data).map(function(item){
 				return {
 					latlng:{lat:item.location.lat,lng:item.location.lng},
-					name:item.name
+					name:item.name,
+					venueid:item.id
 				};
 			});
 
@@ -66,13 +75,31 @@ function getVenues(location,category,icon){
 
 			_(venues).each(function(venue){
 
-				let foodIcon = L.icon({
+				let placeIcon = L.icon({
 					iconUrl:icon,
-					iconSize:[25,25]
+					iconSize:[30,30]
 				});
-				let marker = L.marker(venue.latlng,{icon:foodIcon}).addTo(map);
+				let marker = L.marker(venue.latlng,{icon:placeIcon}).addTo(map);
 
 				marker.venueid = venue.venueid;
+
+				marker.on('click',function(){
+					var venueUrl = 	'https://api.foursquare.com/v2/venues/'+
+					this.venueid+key;
+					
+					$.ajax({
+						url:venueUrl,
+						dataType:'jsonp',
+						success:function(res){
+							console.log(res);
+							app.currentVenue = res.response.venue;
+							// $('.modal-title').text(venue.name);
+							// $('.modal-body').empty();
+
+							$('.modal').modal('show');
+						}
+					});	
+				});
 			});
 		}
 	});
@@ -87,6 +114,7 @@ var app = new Vue({
 	el:'.app',
 	data:{
 		currentLayer:'layer1',
+		currentVenue:{}
 	},
 	methods:{},
 });
